bd4b39eb5ab9bc8ddd1b038483669ea2122b7537
atrinatrin <poyahashti@gmail.com>
2024-12-26 18:37:14 +0330

    Update dir.py

--- YukkiMusic/core/dir.py
+++ YukkiMusic/core/dir.py
@@ -2,97 +2,48 @@
 # Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
 #
 # This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
+# and is released under the "MIT License".
 # Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
 #
+import logging
+import os
+from os import listdir, mkdir
 
-import asyncio
-import shlex
-from typing import Tuple
+from config import TEMP_DB_FOLDER
 
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError
-
-import config
-
-from ..logging import LOGGER
-
-loop = asyncio.get_event_loop_policy().get_event_loop()
-
-
-def install_req(cmd: str) -> Tuple[str, str, int, int]:
-    async def install_requirements():
-        args = shlex.split(cmd)
-        process = await asyncio.create_subprocess_exec(
-            *args,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await process.communicate()
-        return (
-            stdout.decode("utf-8", "replace").strip(),
-            stderr.decode("utf-8", "replace").strip(),
-            process.returncode,
-            process.pid,
-        )
-
-    return loop.run_until_complete(install_requirements())
+# remove all files on startup  that contains these extentions
+files = [
+    ".jpg",
+    ".jpeg",
+    ".mp3",
+    ".m4a",
+    ".mp4",
+    ".webm",
+    ".png",
+]
 
 
-def git():
-    REPO_LINK = config.UPSTREAM_REPO
-    if config.GIT_TOKEN:
-        GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
-        TEMP_REPO = REPO_LINK.split("https://")[1]
-        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
-    else:
-        UPSTREAM_REPO = config.UPSTREAM_REPO
+def dirr():
+    downloads_folder = "downloads"
+    cache_folder = "cache"
 
-    try:
-        repo = Repo()
-        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
-    except GitCommandError:
-        LOGGER(__name__).info(f"Invalid Git Command")
-    except InvalidGitRepositoryError:
-        repo = Repo.init()
-        if "origin" in repo.remotes:
-            origin = repo.remote("origin")
-        else:
-            origin = repo.create_remote("origin", UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head(
-            config.UPSTREAM_BRANCH,
-            origin.refs[config.UPSTREAM_BRANCH],
-        )
-        repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
-            origin.refs[config.UPSTREAM_BRANCH]
-        )
-        repo.heads[config.UPSTREAM_BRANCH].checkout(True)
+    for file in os.listdir():
+        if any(file.endswith(ext) for ext in files):
+            os.remove(file)
 
-        try:
-            repo.create_remote("origin", config.UPSTREAM_REPO)
-        except Exception:
-            pass
+    if downloads_folder not in listdir():
+        mkdir(downloads_folder)
 
-    nrs = repo.remote("origin")
-    nrs.fetch(config.UPSTREAM_BRANCH)
+    if cache_folder not in listdir():
+        mkdir(cache_folder)
 
-    requirements_file = "requirements.txt"
-    diff_index = repo.head.commit.diff("FETCH_HEAD")
+    if TEMP_DB_FOLDER not in listdir():
+        mkdir(TEMP_DB_FOLDER)
 
-    requirements_updated = any(
-        diff.a_path == requirements_file or diff.b_path == requirements_file
-        for diff in diff_index
-    )
+    logging.info("Directories Updated.")
 
-    try:
-        nrs.pull(config.UPSTREAM_BRANCH)
-    except GitCommandError:
-        repo.git.reset("--hard", "FETCH_HEAD")
 
-    if requirements_updated:
-        install_req("pip3 install --no-cache-dir -r requirements.txt")
-
-    LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
+if __name__ == "__main__":
+    dirr()
