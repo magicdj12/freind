9178d539bc99f4c2081452499ca3e3a7ba26ce84
atrinatrin <poyahashti@gmail.com>
2024-12-26 18:21:43 +0330

    Update dir.py

--- YukkiMusic/core/dir.py
+++ YukkiMusic/core/dir.py
@@ -1,44 +1,98 @@
-import logging
-import os
-import sys
-from os import listdir, mkdir
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
 
-from config import TEMP_DB_FOLDER
+import asyncio
+import shlex
+from typing import Tuple
+
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+
+import config
+
+from ..logging import LOGGER
+
+loop = asyncio.get_event_loop_policy().get_event_loop()
 
 
-def dirr():
-    assets_folder = "assets"
-    downloads_folder = "downloads"
-    cache_folder = "cache"
-
-    if assets_folder not in listdir():
-        logging.warning(
-            f"{assets_folder} پوشه ای پیدا نشد لطفا دوباره ریپازیتوری را بگیرید."
+def install_req(cmd: str) -> Tuple[str, str, int, int]:
+    async def install_requirements():
+        args = shlex.split(cmd)
+        process = await asyncio.create_subprocess_exec(
+            *args,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
         )
-        sys.exit()
+        stdout, stderr = await process.communicate()
+        return (
+            stdout.decode("utf-8", "replace").strip(),
+            stderr.decode("utf-8", "replace").strip(),
+            process.returncode,
+            process.pid,
+        )
 
-    for file in os.listdir():
-        if (
-            file.endswith(".jpg")
-            or file.endswith(".jpeg")
-            or file.endswith(".mp3")
-            or file.endswith(".png")
-            or file.endswith(".session")
-            or file.endswith(".session-journal")
-        ):
-            os.remove(file)
-
-    if downloads_folder not in listdir():
-        mkdir(downloads_folder)
-
-    if cache_folder not in listdir():
-        mkdir(cache_folder)
-
-    if TEMP_DB_FOLDER not in listdir():
-        mkdir(TEMP_DB_FOLDER)
-
-    logging.info("Directories Updated.")
+    return loop.run_until_complete(install_requirements())
 
 
-if __name__ == "__main__":
-    dirr()
+def git():
+    REPO_LINK = config.UPSTREAM_REPO
+    if config.GIT_TOKEN:
+        GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
+        TEMP_REPO = REPO_LINK.split("https://")[1]
+        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
+    else:
+        UPSTREAM_REPO = config.UPSTREAM_REPO
+
+    try:
+        repo = Repo()
+        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
+    except GitCommandError:
+        LOGGER(__name__).info(f"Invalid Git Command")
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        if "origin" in repo.remotes:
+            origin = repo.remote("origin")
+        else:
+            origin = repo.create_remote("origin", UPSTREAM_REPO)
+        origin.fetch()
+        repo.create_head(
+            config.UPSTREAM_BRANCH,
+            origin.refs[config.UPSTREAM_BRANCH],
+        )
+        repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
+            origin.refs[config.UPSTREAM_BRANCH]
+        )
+        repo.heads[config.UPSTREAM_BRANCH].checkout(True)
+
+        try:
+            repo.create_remote("origin", config.UPSTREAM_REPO)
+        except Exception:
+            pass
+
+    nrs = repo.remote("origin")
+    nrs.fetch(config.UPSTREAM_BRANCH)
+
+    requirements_file = "requirements.txt"
+    diff_index = repo.head.commit.diff("FETCH_HEAD")
+
+    requirements_updated = any(
+        diff.a_path == requirements_file or diff.b_path == requirements_file
+        for diff in diff_index
+    )
+
+    try:
+        nrs.pull(config.UPSTREAM_BRANCH)
+    except GitCommandError:
+        repo.git.reset("--hard", "FETCH_HEAD")
+
+    if requirements_updated:
+        install_req("pip3 install --no-cache-dir -r requirements.txt")
+
+    LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
